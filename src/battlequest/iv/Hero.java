823f48e15ee0635bfd84d072a910d61b8c6a0a57
dsamuels1 <dsamuels1@udayton.edu>
2017-04-25 16:26:47 -0400

    Update Hero.java

--- src/battlequest/iv/Hero.java
+++ src/battlequest/iv/Hero.java
@@ -11,8 +11,54 @@ package battlequest.iv;
  */
 public class Hero {
    
-    public Hero(){
-        
+   private int myAttack;
+   private double myDefense;
+   private int myLevel;
+   private int myGold;
+   private int myHp;
+   
+    public Hero(int attack, double defense, int gold, int level, int hp){
+       myAttack = attack;
+       myDefense = defense;
+       myLevel = level;
+       myGold = gold;
+       myHp = hp;
+    }
+   
+   public void setAttack(int attack){
+        myAttack = attack;
+    }
+    
+    public int getAttack(){
+        return myAttack;
+    }
+     public void setHp(int hp){
+        myHp = hp;
+    }
+    
+    public int getHp(){
+        return myHp;
+    }
+     public void setGold(int gold){
+        myGold = gold;
+    }
+   
+    public int getGold(){
+        return myGold;
+    }
+     public void setDefense(int defense){
+        myDefense = defense;
+    }
+    
+    public int getDefense(){
+        return mydefense;
+    }
+     public void setLevel(int level){
+        myLevel = level;
+    }
+    
+    public int getLevel(){
+        return myLevel;
     }
     
    public void attack(Enemy enemy){
